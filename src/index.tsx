83e05d3ec95bfc06c1bb1c4b08e6addb3aa8e019
Bahman <bahman.p@mtnirancell.ir>
2022-05-18 00:52:45 +0430

    to create a simple transpiler

--- src/index.tsx
+++ src/index.tsx
@@ -1,13 +1,39 @@
+import * as esbuild from "esbuild-wasm";
 import ReactDOM from "react-dom";
-import {useState} from "react";
+import {useState, useEffect, useRef} from "react";
 
 
 const App = () => {
+    const ref = useRef<any>();
     const [input, setInput] = useState("");
     const [code, setCode] = useState("");
 
-    const onSubmit = () => {
-        console.log(input)
+    const startService = async () => {
+        ref.current = await esbuild.startService({
+            worker: true,
+            wasmURL: "/esbuild.wasm"
+        });
+    }
+
+    useEffect(() => {
+        startService();
+    }, [])
+
+    const onSubmit =  async () => {
+
+        if (!ref.current) {
+            return;
+        }
+
+        console.log(ref.current);
+        
+        const result = await ref.current.transform(input, {
+            loader: "jsx",
+            target: "es2015"
+        });
+        
+        console.log(result);
+        setCode(result.code);
     }
     
     return <div>
