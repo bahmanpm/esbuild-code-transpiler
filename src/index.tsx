3ccd16c930fe0620ec92a3727e90bf127337c8ab
Bahman <bahman.p@mtnirancell.ir>
2022-05-18 00:15:47 +0430

    to create a basic ui

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,22 @@
 import ReactDOM from "react-dom";
+import {useState} from "react";
+
 
 const App = () => {
-    return <h1>hi</h1>
+    const [input, setInput] = useState("");
+    const [code, setCode] = useState("");
+
+    const onSubmit = () => {
+        console.log(input)
+    }
+    
+    return <div>
+        <textarea onChange={(e)=>setInput(e.target.value)}></textarea>    
+        <div>
+            <button onClick={onSubmit}>Submit</button>
+        </div>
+        <pre>{code}</pre>
+    </div>
 }
 
 ReactDOM.render(<App />, document.querySelector("#root"));
